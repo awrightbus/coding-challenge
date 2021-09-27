d47ceb225adc72cfaf7480377c41d2ef4882d929
Antonio Wright <awright@Jonnies-MBP.lan>
2021-09-27 12:33:58 -0500

    basic front end set up

--- spam-reporter/src/App.js
+++ spam-reporter/src/App.js
@@ -1,10 +1,37 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import reports from './data/reports.json'
+import './App.css'
 
 function App() {
+
+const testData = reports.elements;
+
+useEffect(() => {
+  
+},[]);
+
+  
   return (
-    <div className="App">
-     <h1>Hello World!</h1>
+    
+    <div className="app">
+      <h1 className='title'>Spam Reporting System</h1>
+      <div className='reportContainer'>
+          <div className='leftSection'>
+              <p className='leftId'>Id: 721212815245</p>
+              <p>State: Open</p>
+          </div>
+          <div className='middleSection'>
+              <p>Type: Spam</p>
+              <p className='message'>Message: sLOoOoOoooooonggg asss message sLOoOoOoooooonggg asss message sLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss message</p>
+          </div>
+          <div className='rightSection'>
+              <button>Block</button>
+              <button>Resolve</button>
+          </div>
+      </div>
     </div>
+    
   );
 }
 
