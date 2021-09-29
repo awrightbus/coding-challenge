804cf77b9e09d99a497360ccc7f93ba3ee797b17
Antonio Wright <awright@Jonnies-MBP.lan>
2021-09-29 16:27:35 -0500

    fixed up data processing in UI

--- spam-reporter/src/App.js
+++ spam-reporter/src/App.js
@@ -4,30 +4,81 @@ import './App.css'
 
 function App() {
 
+const URL = 'http://localhost:5252'
+const [reportList, setReportList] = useState([])
+
+
+const blockReport = (reportId) => {
+  axios.put(`${URL}/reports/${reportId}/block`, {
+    state:'CLOSED'
+  })
+  .then((res)=> {
+    setReportList(reportList.filter((report)=> report.id !== res.data.id))
+  })
+  .catch((err)=> {
+    console.log(err)
+  })
+}
+
+const resolveReport = (reportId) => {
+  axios.put(`${URL}/reports/${reportId}/resolve`, {
+    resolveState:true
+  })
+  .then((res)=> {
+    axios.get(`${URL}/reports`)
+    .then((res) => {
+      console.log(res.data,'res.data')
+      setReportList(res.data);
+    })
+    .catch((err)=> {
+      console.log(err)
+    })
+  })
+  .catch((err)=> {
+    console.log(err)
+  })
+}
+
+
+useEffect(() => {
+  axios.get(`${URL}/reports`)
+  .then((res) => {
+    console.log(res.data,'res.data')
+    setReportList(res.data);
+  })
+  .catch((err)=> {
+    console.log(err)
+  })
+},[]);
 
 useEffect(() => {
   
-},[]);
+}, [reportList])
 
-  
+console.log(reportList,'report list')
+
   return (
     
     <div className="app">
       <h1 className='title'>Spam Reporting System</h1>
-      <div className='reportContainer'>
-          <div className='leftSection'>
-              <p className='leftId'>Id: 721212815245</p>
-              <p>State: Open</p>
-          </div>
-          <div className='middleSection'>
-              <p>Type: Spam</p>
-              <p className='message'>Message: sLOoOoOoooooonggg asss message sLOoOoOoooooonggg asss message sLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss messagesLOoOoOoooooonggg asss message</p>
-          </div>
-          <div className='rightSection'>
-              <button>Block</button>
-              <button>Resolve</button>
-          </div>
-      </div>
+      { reportList === [] ? '' : reportList.map((report,index) => {
+        return(<div className='reportContainer' key={index}>
+        <div className='leftSection'>
+            <p className='leftId'>Id: {report.id}</p>
+            <p>State: {report?.state}</p>
+        </div>
+        <div className='middleSection'>
+            <p>Type: {report?.reportType}</p>
+            <p className='message'>Message: {report?.message}</p>
+        </div>
+        <div className='rightSection'>
+            <button onClick={() => blockReport(`${report.id}`)}>Block</button>
+            <button onClick={() => resolveReport(`${report.id}`)}>Resolve</button>
+        </div>
+    </div>
+      )})
+      
+     }
     </div>
     
   );
